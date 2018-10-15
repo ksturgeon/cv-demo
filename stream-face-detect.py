359845b6429fe07e8322ee1b7915f05b71a2b963
Kevin Sturgeon <ksturgeon@maprtech.com>
2018-10-15 11:10:06 -0400

    removed one of the images

--- stream-face-detect.py
+++ stream-face-detect.py
@@ -49,7 +49,8 @@ while running:
     
     # To a stream:
     p = Producer({'streams.producer.default.stream': '/demo-streams/processed-images'})
-    json_data = json.loads(msg.value())['$$document']
+    #json_data = json.loads(msg.value())['$$document']
+    json_data = json.dumps({})
     json_data['processed_image'] = base64.b64encode(image)
     json_data['num_faces']=len(faces)
     p.produce('topic1', json.dumps(json_data))
